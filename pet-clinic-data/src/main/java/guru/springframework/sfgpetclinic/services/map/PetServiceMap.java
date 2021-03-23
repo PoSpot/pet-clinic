507f5b61cffce83ac63b4bb31f76ea488185d331
Po <pmarinova@gmail.com>
2021-03-23 23:52:16 +0200

    Added pets and coordinates on bootstrap. Closes #33

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -2,8 +2,32 @@ package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PetServiceMap extends BaseMapService<Pet> implements PetService {
+
+    private final PetTypeService petTypeService;
+
+    public PetServiceMap(PetTypeService petTypeService) {
+        this.petTypeService = petTypeService;
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+
+        if (pet.getPetType() != null) {
+            if (pet.getPetType().getId() == null){
+                // !! no need to set the saved type
+                petTypeService.save(pet.getPetType());
+            }
+        } else {
+            throw new IllegalArgumentException("Pet Type is required on Pet entity");
+        }
+
+
+
+        return super.save(pet);
+    }
 }
