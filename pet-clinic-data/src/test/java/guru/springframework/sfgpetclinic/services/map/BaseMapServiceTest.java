9bffeb242ca46518b1805161a53058737d1425f0
Po <pmarinova@gmail.com>
2021-04-06 08:58:07 +0300

    ups #49

--- pet-clinic-data/src/test/java/guru/springframework/sfgpetclinic/services/map/BaseMapServiceTest.java
+++ pet-clinic-data/src/test/java/guru/springframework/sfgpetclinic/services/map/BaseMapServiceTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class BaseMapServiceTest extends BaseMapService {
+class BaseMapServiceTest {
 
     PetTypeMapService service;
 
